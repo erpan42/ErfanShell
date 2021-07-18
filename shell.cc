339acde6a741014de11af9388135b601effd26a8
erpan42 <yierpan42@gmail.com>
2021-07-18 14:59:20 -0400

    Commit

--- shell.cc
+++ shell.cc
@@ -15,20 +15,7 @@ void Shell::prompt() {
 	fflush(stdout);
 }
 
-//when ctrl+C on ongoing process, nextline is myshell>>myshell>>
-extern "C" void ctrlC(int sig) {
-	//fflush(stdin);
-	printf("\n");
-	Shell::prompt();
-}
 
-//TODO: Only one msg for the same PID
-extern "C" void zombie(int sig) {
-	int pid = wait3(0, 0, NULL);
-
-	printf("[%d] exited.\n", pid);
-	while (waitpid(-1, NULL, WNOHANG) > 0) {};
-}
 
 int main() {
 	
