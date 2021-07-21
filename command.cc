de50106f7d4733cc3c646047d2974871c170ab7a
erpan42 <yierpan42@gmail.com>
2021-07-21 16:50:39 -0400

    Commit

--- command.cc
+++ command.cc
@@ -121,11 +121,9 @@ bool Command::builtIn(int i) {
 		int error;
 
 		if (_simpleCommands[i]->_arguments.size() == 1) {	//if only "cd", then go HOME
-			error = chdir(getenv("HOME"));
 			printf("%s\n",getenv("HOME"));
 		}else {
 
-			printf("%s\n",_simpleCommands[i]->_arguments[1]->c_str());
 			error = chdir(_simpleCommands[i]->_arguments[1]->c_str());
 		}
 
