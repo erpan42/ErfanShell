281bdb0a3d9023ffa5fd11904de831b78e196718
erpan42 <yierpan42@gmail.com>
2021-07-21 17:23:15 -0400

    Commit

--- command.hh
+++ command.hh
@@ -2,44 +2,27 @@
 #define command_hh
 
 #include "simpleCommand.hh"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <string.h>
-#include <signal.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <regex.h>
-#include <pwd.h>
 
 // Command Data Structure
 
 struct Command {
-
-
   std::vector<SimpleCommand *> _simpleCommands;
   std::string * _outFile;
   std::string * _inFile;
   std::string * _errFile;
+  bool _background;
 
-  //User Add-on
-  bool _background;	//run in background or not
-  int _append;	//if ">>", then _append = 1
+  bool _append;
 
   Command();
   void insertSimpleCommand( SimpleCommand * simpleCommand );
 
   void clear();
   void print();
+  void redirect(int i, std::string * curr);
   void execute();
-  
+
   static SimpleCommand *_currentSimpleCommand;
-  //i is the index of commandlist
-  bool builtIn(int i);	
-  //int i  is the index of commandlist.printenv and source
-  bool builtIn2(int i);	
 };
 
 #endif
--- shell.hh
+++ shell.hh
@@ -2,12 +2,17 @@
 #define shell_hh
 
 #include "command.hh"
+#include <vector>
 
 struct Shell {
 
   static void prompt();
 
   static Command _currentCommand;
+
+  static std::vector<int> _bgPIDs;
+
+  static bool _srcCmd;
 };
 
 #endif
