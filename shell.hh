73f82800d18930ef52e3f43ae591dd3f446a4086
erpan42 <yierpan42@gmail.com>
2021-07-21 17:19:50 -0400

    Commit

--- shell.hh
+++ shell.hh
@@ -2,17 +2,12 @@
 #define shell_hh
 
 #include "command.hh"
-#include <vector>
 
 struct Shell {
 
   static void prompt();
 
   static Command _currentCommand;
-
-  static std::vector<int> _bgPIDs;
-
-  static bool _srcCmd;
 };
 
 #endif
