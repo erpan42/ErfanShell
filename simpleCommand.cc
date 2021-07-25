c358913e674fd82aba3c6d587854efe991ecc241
erpan42 <yierpan42@gmail.com>
2021-07-25 19:51:15 -0400

    Commit

--- simpleCommand.cc
+++ simpleCommand.cc
@@ -1,13 +1,14 @@
 #include <cstdio>
 #include <cstdlib>
-#include <string.h>
-#include <iostream>
 
+#include <iostream>
+#include <string.h>
 #include "simpleCommand.hh"
 
-SimpleCommand::SimpleCommand() 
-{
+SimpleCommand::SimpleCommand() {
   _arguments = std::vector<std::string *>();
+  envtrue = false;
+  tildtrue = false;
 }
 
 SimpleCommand::~SimpleCommand() {
@@ -17,59 +18,68 @@ SimpleCommand::~SimpleCommand() {
   }
 }
 
-void SimpleCommand::insertArgument( std::string * argument ) {
-  // simply add the argument to the vector
-	int i=0;
-	char * str = strdup(argument -> c_str());
-	char * arg = strdup(argument -> c_str());
-	//char * str = (char *)malloc(strlen(argument)+1);
-	char * text = str;
-	bool esc = false;
-	if(*text == '~')
-	{
+//Environement Variable Expansion
+//Environement Variable Expansion
+std::string * SimpleCommand::envexpansion(std::string * argument) {
+	char *arg = const_cast<char*> (argument->c_str());
+
+	if ( arg[0]== '$') {
+		//Get rid of ${ and }
+		char * realarg = (char*)malloc(200);
+		char *realargptr = realarg;
+		arg = arg + 2;
+		while (*arg != '}') {
+			*realargptr = *arg;
+			realargptr++;
+			arg++;
+		}
+		*realargptr = '\0';
+		printf("the argument is : %s\n", realarg);
+
+		//Get environment variable
+		char *arg = const_cast<char*> (argument->c_str());
+		char * env = getenv(arg);
+		printf("the environment variable is : %s\n", env);
+
+		//Append character by character? How to do?
+		std::string *str;
+
+		//Append char by char? Not working
+		while (*env) {
+			//no matching function for call to ¡®std::__cxx11::basic_string<char>::append(char&)
+			//invalid conversion from ¡®char¡¯ to ¡®const char*¡¯
+			str->append(env);			
+		}
 		
+		return str;
+
+		//str = argument->substr(2);
+		//std::size_t	pos = str->find(")");
+		//str = str->substr(0, pos);
+		//char *temp = const_cast<char*> (str->c_str());
+		//printf("the argument is : %s\n", temp);
 	}
-	else{
-	while(*text!='\0')
-	{
-		if(!esc && *text == '\\')
-		{
-			esc = true;
-		}
-		else
-		{
-			str[i] = *text;
-			i++;
-			esc = false;
-		}
-		text++;
-	}
-	str[i] = '\0';
-	}
-	
-	std::string * pb = new std::string(str);
-	_arguments.push_back(pb);
-	free(str);
-	delete argument;
+	envtrue = true;
+	return argument;
+}
 
-/*
-	//Environment variable expansion
-	char * env = expansion(arg);
-	if(env)
-		arg = strdup(env);
+void SimpleCommand::insertArgument( std::string * argument ) {
+	// char *arg = const_cast<char*> (argument->c_str());
+	// printf("the argument to be inserted is: %s\n", arg);
 
-	env = tilde(arg);
+	// //3.1: environment variable expansion
+	// std::string * envexp = envexpansion(argument);
 
-	if(env) 
-		arg = strdup(env);
+	// if (envtrue == true) {
+	// 	argument = envexp;
+	// }
 
-	_arguments[ argcount ] = arg;
+	// //3.2: tilde expansion
 
-	// Add NULL argument at the end
-	_arguments[ argcount + 1] = NULL;
 
-	argcount++;
-*/
+	envtrue = false;
+	tildtrue = false;
+	_arguments.push_back(argument);
 }
 
 // Print out the simple command
