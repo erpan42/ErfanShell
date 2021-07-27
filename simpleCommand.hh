9c8a32b94718d898105ce8e4dca19fc705840751
erpan42 <yierpan42@gmail.com>
2021-07-27 17:32:47 -0400

    Commit

--- simpleCommand.hh
+++ simpleCommand.hh
@@ -1,42 +1,18 @@
-#ifndef simplcommand_h
-#define simplecommand_h
+#ifndef simplecommand_hh
+#define simplecommand_hh
 
-//C++
 #include <string>
-#include <algorithm>
-#include <iterator>
 #include <vector>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include "y.tab.hh"
-
-//C
-#include <dirent.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <signal.h>
-#include <regex.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <pwd.h>
-#define MAXFILENAME 1024
 
 struct SimpleCommand {
-  // Available space for arguments currently preallocated
-  int _numOfAvailableArguments;
 
-  // Number of arguments
-  int _numOfArguments;
-  char ** _arguments;
+  // Simple command is simply a vector of strings
+  std::vector<std::string *> _arguments;
 
   SimpleCommand();
-  void insertArgument( char * argument );
-  char * checkExpansion(char * argument);
-  char * tilde(char * argument);
+  ~SimpleCommand();
+  void insertArgument( std::string * argument );
+  void print();
 };
 
 #endif
