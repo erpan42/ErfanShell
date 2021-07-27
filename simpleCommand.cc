7756d69bd9a24c4790ac4b5c5d77d1fbb0685126
erpan42 <yierpan42@gmail.com>
2021-07-27 17:25:08 -0400

    Commit

--- simpleCommand.cc
+++ simpleCommand.cc
@@ -1,31 +1,133 @@
-#include <cstdio>
 #include <cstdlib>
-
-#include <iostream>
 
 #include "simpleCommand.hh"
 
 SimpleCommand::SimpleCommand() {
-  _arguments = std::vector<std::string *>();
+	// Create available space for 5 arguments
+	_numOfAvailableArguments = 5;
+	_numOfArguments = 0;
+	_arguments = (char **) malloc( _numOfAvailableArguments * sizeof( char * ) );
 }
 
-SimpleCommand::~SimpleCommand() {
-  // iterate over all the arguments and delete them
-  for (auto & arg : _arguments) {
-    delete arg;
-  }
+char * SimpleCommand::checkExpansion(char * argument) {
+	char * arg = strdup(argument);
+	char * checkDollar = strchr(arg, '$');
+	char * checkBraces = strchr(arg, '{');
+
+	char * replace = (char *) malloc (sizeof(argument) + 50);
+	char * temp = replace;
+
+	if (checkDollar && checkBraces) {
+		while (*arg != '$') {
+			*temp = *arg;
+			temp++; arg++;
+		}
+		*temp = '\0';
+
+		while (checkDollar) {
+			if (checkDollar[1] == '{' && checkDollar[2] != '}') {
+				char * temporary = checkDollar + 2;
+				char * env = (char *) malloc (20);
+				char * envtemp = env;
+
+				while (*temporary != '}') {
+					*envtemp = *temporary;
+					envtemp++; temporary++;
+				}
+				*envtemp = '\0';
+
+				char * get = getenv(env);
+
+				strcat(replace, get);
+
+				while (*(arg-1) != '}') arg++;
+
+				char * buf = (char *) malloc (20);
+				char * tbuf = buf;
+
+				while (*arg != '$' && *arg) {
+					*tbuf = *arg;
+					tbuf++; arg++;
+				}
+				*tbuf = '\0';
+				strcat(replace, buf);
+			}
+			checkDollar++;
+			checkDollar = strchr(checkDollar, '$');
+		}
+		argument = strdup(replace);
+		return argument;
+	}
+	return NULL;
 }
 
-void SimpleCommand::insertArgument( std::string * argument ) {
-  // simply add the argument to the vector
-  _arguments.push_back(argument);
+char * SimpleCommand::tilde(char * argument) {
+	if (argument[0] == '~') {
+		
+		if (strlen(argument) == 1) {
+		
+			argument = strdup(getenv("HOME"));
+			return argument;
+		
+		} else {
+
+			if (argument[1] == '/') {
+				char * dir = strdup(getenv("HOME"));
+				argument++;
+				argument = strcat(dir, argument);
+				return argument;
+			}
+
+			char * nArgument = (char *) malloc (strlen(argument) + 20);
+			char * uName = (char *) malloc (50);
+			char * user = uName;
+			char * temp = argument;
+
+			temp++;
+
+			while (*temp != '/' && *temp) *(uName++) = *(temp++);
+			*uName = '\0';
+
+
+			if (*temp) {
+
+				nArgument = strcat(getpwnam(user)->pw_dir, temp);
+				argument = strdup(nArgument);
+				return argument;
+
+			} else {
+				
+				argument = strdup(getpwnam(user)->pw_dir);
+				return argument;
+
+			}
+		}
+
+	}
+
+	return NULL;
 }
 
-// Print out the simple command
-void SimpleCommand::print() {
-  for (auto & arg : _arguments) {
-    std::cout << "\"" << *arg << "\" \t";
-  }
-  // effectively the same as printf("\n\n");
-  std::cout << std::endl;
+void SimpleCommand::insertArgument( char * argument ) {
+	if ( _numOfAvailableArguments == _numOfArguments  + 1 ) {
+		// Double the available space
+		_numOfAvailableArguments *= 2;
+		_arguments = (char **) realloc( _arguments,
+				  _numOfAvailableArguments * sizeof( char * ) );
+	}
+	// Variable expansion implementation
+	char * exp = checkExpansion(argument);
+	if (exp) 
+		argument = strdup(exp);
+
+	exp = tilde(argument);
+	if (exp) 
+		argument = strdup(exp);
+
+	_arguments[ _numOfArguments ] = argument;
+
+	// Add NULL argument at the end
+	_arguments[ _numOfArguments + 1] = NULL;
+	
+	_numOfArguments++;
 }
--- simpleCommand.hh
+++ simpleCommand.hh
@@ -1,18 +1,42 @@
-#ifndef simplecommand_hh
-#define simplecommand_hh
+#ifndef simplcommand_h
+#define simplecommand_h
 
+//C++
 #include <string>
+#include <algorithm>
+#include <iterator>
 #include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "y.tab.hh"
+
+//C
+#include <dirent.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <regex.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <pwd.h>
+#define MAXFILENAME 1024
 
 struct SimpleCommand {
+  // Available space for arguments currently preallocated
+  int _numOfAvailableArguments;
 
-  // Simple command is simply a vector of strings
-  std::vector<std::string *> _arguments;
+  // Number of arguments
+  int _numOfArguments;
+  char ** _arguments;
 
   SimpleCommand();
-  ~SimpleCommand();
-  void insertArgument( std::string * argument );
-  void print();
+  void insertArgument( char * argument );
+  char * checkExpansion(char * argument);
+  char * tilde(char * argument);
 };
 
 #endif
