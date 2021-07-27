ebfdf13dcdced5ebfe3ecb973277af320e62f37b
erpan42 <yierpan42@gmail.com>
2021-07-27 17:44:20 -0400

    Commit

--- simpleCommand.hh
+++ simpleCommand.hh
@@ -3,6 +3,27 @@
 
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "y.tab.hh"
+
+//C
+#include <dirent.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <regex.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <pwd.h>
+#define MAXFILENAME 1024
+
 
 struct SimpleCommand {
 
@@ -12,6 +33,8 @@ struct SimpleCommand {
   SimpleCommand();
   ~SimpleCommand();
   void insertArgument( std::string * argument );
+  char * checkExpansion(char * argument);
+  char * tilde(char * argument);
   void print();
 };
 
